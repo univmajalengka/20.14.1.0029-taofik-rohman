196d93b415e8ee620d516348617e6ba9b173a442
wina siti aditianingsih <rohmantaofik417@gmail.com>
2021-03-20 22:15:04 +0700

    tugas 1

---
+++ biodata singkat.pas
@@ -0,0 +1,15 @@
+program biodatasingakt;
+uses crt;
+
+begin
+writeln('biodata');
+writeln('Nama  :taofik rohman');
+writeln('NPM   :20.14.1.0029');
+writeln('alamat:blok heuleut desa cieurih kecamatan maja');
+writeln('TTL   :Majalengka, 28 oktober 2000');
+writeln('NO hp :081572374991');
+writeln('motto :renungkan walaupun cuma sekajap saja');
+
+
+readln;
+end.
