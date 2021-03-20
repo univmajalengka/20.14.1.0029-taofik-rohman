27068fb80276020262270697563be1768b660999
wina siti aditianingsih <rohmantaofik417@gmail.com>
2021-03-20 22:11:12 +0700

    tugas 1

---
+++ biodata.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int i,j,hasil;
+	
+	cout << "Nama	: taofik rohman"<< endl;
+	cout << "NPM	: 20.14.1.0029"<< endl;
+	cout << "Alamat	: blok heuleut desa cieurih kec maja kab majalengka"<< endl;
+	cout << "TTL	: Majalengka, 28 okotober 2000"<< endl;
+	cout << "No. HP	: 081572374991"<< endl;
+	cout << "Motto	: renungkan walaupun cuma sekejap saja"<< endl;
+	
+	return 0;
+}
