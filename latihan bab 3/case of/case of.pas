e075a72b324bf7284f7a8df4f661c6866821e908
wina siti aditianingsih <rohmantaofik417@gmail.com>
2021-03-22 19:16:04 +0700

    bab 3

---
+++ latihan bab 3/case of/case of.pas
@@ -0,0 +1,37 @@
+{nama           :taofik rohman  }
+{NPM            :201410029      }
+{nama program   :case_of.pas    }
+
+program case_of;
+uses crt;
+
+var
+nama : string;
+gol : char;
+jmlanak : integer;
+upahkotor,upahbersih : real ;
+persentunjangan : real ;
+
+begin
+clrscr;
+        write('nama : '); readln(nama);
+        write('gol  (A-D) :');readln(gol);
+        write('jmlanak: '); readln(jmlanak);
+
+        case gol of
+        'A' : upahkotor:=1000000;
+        'B' : upahkotor:=800000;
+        'C' : upahkotor:=600000;
+        'D' : upahkotor:=400000;
+        end;
+
+        if(jmlanak > 2) then
+        persentunjangan := 0.3
+        else
+        persentunjangan := 0.2;
+
+        upahbersih:= upahkotor - (upahkotor*persentunjangan);
+        writeln('upah : ', upahbersih:10:2);
+        readln;
+
+end.
